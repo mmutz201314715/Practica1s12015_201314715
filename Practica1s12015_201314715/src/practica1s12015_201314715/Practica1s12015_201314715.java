dfde0c0d4d8ff42de7d49d40bc5334e8b302bbd1
mariopc <mariopc@mario>
2015-02-21 23:57:49 -0600

    prubea modificacion 1

--- Practica1s12015_201314715/src/practica1s12015_201314715/Practica1s12015_201314715.java
+++ Practica1s12015_201314715/src/practica1s12015_201314715/Practica1s12015_201314715.java
@@ -15,7 +15,7 @@ public class Practica1s12015_201314715 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo 2");
         // TODO code application logic here
     }
     
