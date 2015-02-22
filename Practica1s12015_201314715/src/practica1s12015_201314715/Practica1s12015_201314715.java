644c3bf703494f97b8fcd0789724773d639cbd40
mariopc <mariopc@mario>
2015-02-21 23:48:21 -0600

    prueba de archivos en git

---
+++ Practica1s12015_201314715/src/practica1s12015_201314715/Practica1s12015_201314715.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1s12015_201314715;
+
+/**
+ *
+ * @author mariopc
+ */
+public class Practica1s12015_201314715 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        // TODO code application logic here
+    }
+    
+}
