21553346144e3368e0fcde8ca38207f90d8dc109
mmutz2015 <mmutzusac@gmail.com>
2015-02-22 00:00:29 -0600

    Update Practica1s12015_201314715.java
    prueba pull

--- Practica1s12015_201314715/src/practica1s12015_201314715/Practica1s12015_201314715.java
+++ Practica1s12015_201314715/src/practica1s12015_201314715/Practica1s12015_201314715.java
@@ -15,7 +15,7 @@ public class Practica1s12015_201314715 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo 2");
+        System.out.println("hola mundo 3");
         // TODO code application logic here
     }
     
